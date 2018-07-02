3a0db91e34ec4c6ea671e7d27d748a792d1bc91f
Sushi Chanrai <sushi@pathworks.co>
2018-07-02 16:56:49 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,9 +2,9 @@ def my_each(array) # put argument(s) here
   i = 0
     while i < array.length
       yield array[i]
-      i++
+      i +=
     end
   array
 end
 
-#my_each(array) { |k| puts k } 
+#my_each(array) { |k| puts k }
