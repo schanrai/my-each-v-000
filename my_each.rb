d4b3fb197b63aab78e059134ea59523543e95f00
Sushi Chanrai <sushi@pathworks.co>
2018-07-02 17:23:03 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,10 @@
-def my_each(array) # put argument(s) here
+def my_each(words) # put argument(s) here
   i = 0
-    while i < array.length
-      yield array[i]
+    while i < words.length
+      yield word[i]
       i = i + 1
     end
   array
 end
 
-my_each(['hi', 'hello', 'bye', 'goodbye']) do k puts k end
+#my_each(['hi', 'hello', 'bye', 'goodbye']) do k puts k end
