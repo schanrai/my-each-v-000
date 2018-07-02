fec3176909466131723e8fb28b495d751d3585be
Sushi Chanrai <sushi@pathworks.co>
2018-07-02 17:58:20 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(words)
+  i = 0
+    while i < words.length
+      yield words[i]
+      i = i + 1
+    end
+  words
+end
+
+
+my_each([1,2,3]) do |k|
+  array = Array.new
 end
